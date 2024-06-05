988ff34176e1afb3876f0dca9c476bba64d368a1
kynajain <kyna.jain10@gmail.com>
2024-06-05 15:49:04 +0100

    Initial Commit

---
+++ Buildr/BuildrApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuildrApp.swift
+//  Buildr
+//
+//  Created by Kyna Jain on 05/06/24.
+//
+
+import SwiftUI
+
+@main
+struct BuildrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
